064682b1fca5c8d45330a41124ca4e8f9cbf5eb0
kusuma M <mkusuma206@gmail.com>
2026-02-10 12:48:10 +0530

    completed till user input

---
+++ user_input.java
@@ -0,0 +1,47 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class user_input_string{
+    public static void main(String[] args ) {
+        Scanner scn = new Scanner(System.in);
+        String name = scn.nextInt();
+        System.out.println("The name of a is "+a);
+    }
+}
+
+
+import java.lang.System;
+import java.util.Scanner;
+
+class user_input_string{
+    public static void main(String[] args ) {
+        Scanner scn = new Scanner(System.in);
+        String name = scn.nextLine();
+        System.out.println("The name of a is "+name);
+    }
+}
+
+class user_input_string{
+
+}
+class user_input_float{
+    public static void main(String[] args ) {
+        Scanner scn = new Scanner(System.in);
+        float f = scn.nextFloat();
+        System.out.println("The value of f is : "+f)
+    } 
+}
+
+
+class addition_of_two_numbers{
+    public static void main(String[] args ) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a= scn.nextInt();
+        System.out.println("Enter second number: ");
+        int b=scn.nextInt();
+        int sum=a+b;
+        System.out.println("The sum of "+a+" and "+b+" is: "+sum);
+    }
+}
+
